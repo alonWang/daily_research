13963ccd34d0ec843b49673b137266184cf0c9fc
wangweilong <wangweilong1996@gmail.com>
2017-10-04 14:45:29 +0800

    add

---
+++ leetcode/Q3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set=new HashSet<Character>();
+        
+        int i=0,j=0;
+        int n=s.length();
+        int ans=0;
+        while(i<n&&j<n){
+            
+            if(set.contains(s.charAt(j))){
+                set.remove(s.charAt(i++));
+            }else{
+                set.add(s.charAt(j++));
+                ans=Math.max(j-i,ans);
+            }
+            
+        }
+       return ans; 
+    }
+}
---
+++ leetcode/Q300.java
@@ -0,0 +1,57 @@
+//递归版本 n^2
+class Solution {
+    int[] p=new int[10000];
+    int[] cache=new int[10000];
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            p[i]=nums[i];
+        }
+        p[n]=100000;
+        return robot(n)-1;
+    }
+    public int robot(int idx){
+        if(-1==idx)
+            return 0;
+        if(0!=cache[idx])
+            return cache[idx];
+        int ans=0;
+        for(int i=0;i<idx;i++){
+            if(p[idx]>p[i]){
+                ans=Math.max(ans,robot(i));    
+            }
+        }
+        cache[idx]=ans+1;
+        return ans+1;
+        
+    }
+}
+
+//非递归版本
+class Solution {
+    int[] p=new int[10000];
+    int[] cache=new int[10000];
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        if(0==n){
+            return 0;
+        }
+        for(int i=0;i<nums.length;i++){
+            p[i]=nums[i];
+        }
+        p[n]=100000;
+        cache[0]=0;
+        for(int idx=0;idx<=n;idx++){
+            int ans=0;
+            for(int i=0;i<idx;i++){
+                if(p[idx]>p[i]){
+                    ans=Math.max(ans,cache[i]);    
+                }
+            }
+            cache[idx]=ans+1;
+        }
+        return cache[n]-1;
+        
+    }
+
+}
