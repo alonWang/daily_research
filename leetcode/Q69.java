c8097f2df996761e8164f925ecbb46f9ac32d5c0
wangweilong <wangweilong1996@gmail.com>
2017-10-06 18:38:48 +0800

    二分搜索

---
+++ leetcode/Q69.java
@@ -0,0 +1,20 @@
+class Solution {
+    boolean guess(long m,long x){
+        return m*m <= x;
+    }
+    public int mySqrt(int x) {
+        //这里的强转确保x为Integer.MAX_VALUE时不会溢出
+        long l=0,r= (long)x+1;
+        long ans=0;
+        while(l<r){
+            long mid=(l+r)/2;
+            if(guess(mid,x)){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid;
+            }
+        }
+       return (int)ans; 
+    }
+}
