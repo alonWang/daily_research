f9e7a0426cd843928f624a7f90f9ff789d0eaffa
velo <wangweilong1996@gmail.com>
2017-10-08 09:33:23 +0800

    优化

--- leetcode/Q33.java
+++ leetcode/Q33.java
@@ -5,13 +5,11 @@ class Solution {
         if(n<1)
             return -1;
         //分割点
-        int i=0;
-        while(i<n-1&&nums[i]<nums[i+1])
-            i++;
+        int pivot=findPivot(nums);
       
-        int ans=guess(0,i+1,target,nums);
+        int ans=guess(0,pivot,target,nums);
         if(ans==-1)
-            ans=guess(i+1,n,target,nums);
+            ans=guess(pivot,n,target,nums);
         return ans;
     }
     //二分搜索
@@ -29,5 +27,22 @@ class Solution {
             }
         }
        return ans;
+   }
+   public int findPivot(int[] nums){
+    int n=nums.length;
+    
+     int L=0,R=n;
+     int target=nums[0];
+     int ans=0;
+     while(L<R){
+         int mid=(L+R)/2;
+         if(nums[mid]<target){
+             ans=mid;
+             R=mid;
+         }else{
+             L=mid+1;
+         }
+     }
+     return ans;
    }
 }
