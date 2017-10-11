21ff9bf68d2625fb48d481eb17c6199d8d8b428e
velo <wangweilong1996@gmail.com>
2017-10-11 20:15:58 +0800

    longest common prefix

---
+++ leetcode/Q14.java
@@ -0,0 +1,31 @@
+/**
+ * 以第一个字符串为前缀,不断缩短长度,与剩余字符串比较.
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length<1)
+            return "";
+        String prefix=new String(strs[0]);
+        
+        while(prefix.length()>0){
+            boolean flag=true;
+            for(int i=1;i<strs.length;i++){
+                if(!strs[i].startsWith(prefix)){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag)
+                return prefix;
+            
+            
+            if(prefix.length()==1)
+                prefix="";
+            else
+                prefix=prefix.substring(0,prefix.length()-1);
+            
+        }
+        return prefix; 
+    }
+    
+}
--- subsequence/LCS.java
+++ subsequence/LCS.java
@@ -1,9 +1,5 @@
 package com.velo.algorithm4.subsequence;
-
 /**
- * 最长公共子序列lcs
- * 时间复杂度 O(mn),空间复杂度 O(2mn) (如果把轨迹数组优化一下可以达到O(mn))
- * 自己尝试描述了一下,不是很满意,请看作者的讲解
  * http://blog.csdn.net/v_JULY_v/article/details/6110269
  */
 public class LCS {
@@ -12,24 +8,17 @@ public class LCS {
     public static void main(String[] args) {
         String s1 = "ACBFGXXXXFXXXXXXXXX";
         String s2 = "AHUJCJKBHIFJIGVF";
-        //trace[i][j]表示dp[i][j]是经  `1` dp[i-1][j-1]    `2` dp[i][j-1]   `3`  dp[i-1][j] 得到的
+        //trace[i][js means dp[i][j] is  `1` dp[i-1][j-1]    `2` dp[i][j-1]   `3`  dp[i-1][j] 
         int[][] trace = longestCommonSequence(s1, s2);
         showLcs(trace, s1.length(), s2.length(), s1);
 
     }
 
-    /**
-     * 设置轨迹数组
-     *
-     * @param s1
-     * @param s2
-     * @return 轨迹数组
-     * 输出最长子序列长度
-     */
+
     public static int[][] longestCommonSequence(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
-        //dp[i][j]表示s1[0~i]、s2[0~j]时的最长公共子序列
+        //dp[i][j] means s1[0~i]、s2[0~j] lcs
         int[][] dp = new int[m + 1][n + 1];
         int[][] trace = new int[m + 1][n + 1];
 
@@ -55,27 +44,10 @@ public class LCS {
         return trace;
     }
 
-    /**
-     * 输出最长子序列
-     *
-     * @param trace
-     * @param i     s1长度
-     * @param j     s2长度
-     * @param s     s1、s2任意
-     */
+
     public static void showLcs(int[][] trace, int i, int j, String s) {
-        //递归
-        /*if(i==0||j==0)return;
-        else if(trace[i][j]==1){
-            showLcs(i-1,j-1,s);
-            System.out.print(s.charAt(i-1));
-        }else{
-            if(trace[i][j]==2)
-                showLcs(i,j-1,s);
-            else if(trace[i][j]==3)
-                showLcs(i-1,j,s);
-        }*/
-        //非递归
+
+
         StringBuilder sb = new StringBuilder();
         while (i > 0 && j > 0) {
             if (trace[i][j] == 1) {
