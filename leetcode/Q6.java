bf0e19774d21c43e8bb685e60864ae2ee79e3e0d
velo <wangweilong1996@gmail.com>
2017-11-11 15:26:34 +0800

    zigzag

---
+++ leetcode/Q6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows<1)
+            return "";
+        
+        StringBuilder []sb=new StringBuilder[numRows];
+        for(int i=0;i<numRows;i++)
+            sb[i]=new StringBuilder();
+        
+        int index=0;
+        while(index<s.length()){
+            for(int i=0;i<numRows&&index<s.length();i++)
+                sb[i].append(s.charAt(index++));
+            for(int i=numRows-2;i>0&&index<s.length();i--)
+                sb[i].append(s.charAt(index++));
+            
+            
+        }
+        for(int i=1;i<numRows;i++)
+            sb[0].append(sb[i]);
+        return sb[0].toString();
+    }
+}
