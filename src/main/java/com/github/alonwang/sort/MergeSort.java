0739c0f332a8f7f2b32a990e50126a2c62767154
alonwang <wangweilong1996@gmail.com>
2018-08-16 21:45:31 +0800

    merge sort optimise

--- src/main/java/com/github/alonwang/sort/MergeSort.java
+++ src/main/java/com/github/alonwang/sort/MergeSort.java
@@ -1,8 +1,10 @@
 package com.github.alonwang.sort;
 
 
+import java.util.Arrays;
+
 public class MergeSort extends CompareTemplate {
-    public static Comparable[] aux;
+    private static Comparable[] assist;
 
     public static void main(String[] args) {
         Integer a[] = new Integer[]{1, 5, 3, 4, 7, 2, 4, 6};
@@ -15,36 +17,41 @@ public class MergeSort extends CompareTemplate {
 
     @Override
     public void sort(Comparable[] a) {
-        aux = new Comparable[a.length];
+        assist = new Comparable[a.length];
         sort(a, 0, a.length - 1);
     }
 
-    //左右全闭合
-    public void sort(Comparable[] a, int lo, int hi) {
+    // mainly for control the invoke order of multi merge()
+    public void sort(Comparable[] sources, int lo, int hi) {
         if (hi <= lo) return;
         int mid = lo + (hi - lo) / 2;
-        sort(a, lo, mid);
-        sort(a, mid + 1, hi);
-        merge(a, lo, mid, hi);
+        sort(sources, lo, mid);
+        sort(sources, mid + 1, hi);
+        merge(sources, lo, mid, hi);
 
     }
 
-    public void merge(Comparable[] a, int lo, int mid, int hi) {
+    /**
+     * left and right must bu ordered individual
+     *
+     * @param sources
+     * @param lo
+     * @param mid
+     * @param hi
+     */
+    public void merge(Comparable[] sources, int lo, int mid, int hi) {
         //前后半段分别处理
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++)
-            aux[k] = a[k];
+        int lIdx = lo, rIdx = mid + 1;
+        assist = Arrays.copyOf(sources, sources.length);
         /*
-        如果前半段已处理完,处理后半段
-        否则 如果后半段已处理完,处理前半段
-        否则 如果后半段小于前半段,处理后半段
-        否则 处理前半段
+            if  the left or right has over,then just add another side to assist by order
+            nor find the smaller in two side of their current pos and put it in the assist
          */
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (less(aux[j], aux[i])) a[k] = aux[j++];
-            else a[k] = aux[i++];
+        for (int assIdx = lo; assIdx <= hi; assIdx++) {
+            if (lIdx > mid) sources[assIdx] = assist[rIdx++];
+            else if (rIdx > hi) sources[assIdx] = assist[lIdx++];
+            else if (less(assist[rIdx], assist[lIdx])) sources[assIdx] = assist[rIdx++];
+            else sources[assIdx] = assist[lIdx++];
         }
     }
 }
