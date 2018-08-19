711f43321b8ae62fc27f714dbc54dc554c8b745d
alonwang <wangweilong1996@gmail.com>
2018-08-19 22:01:05 +0800

    Q147 complete

--- src/main/java/com/github/alonwang/leetcode/Q147.java
+++ src/main/java/com/github/alonwang/leetcode/Q147.java
@@ -2,67 +2,59 @@ package com.github.alonwang.leetcode;
 
 /**
  * @author weilong.wang Created on 2018/8/17
+ * <p>
+ * refer https://blog.csdn.net/happyaaaaaaaaaaa/article/details/51619973
  */
 
 class ListNode {
-	int val;
-	ListNode next;
+    int val;
+    ListNode next;
 
-	ListNode(int x) {
-		val = x;
-	}
+    ListNode(int x) {
+        val = x;
+    }
 }
 
 public class Q147 {
-	public static ListNode insertionSortList(ListNode head) {
-		ListNode headNode = head;
-		ListNode formerNode = headNode;
-		ListNode curNode = formerNode.next;
-		while (curNode != null) {
-			// remove node
-			ListNode latterNode = curNode.next;
-			formerNode.next = latterNode;
-			curNode.next = null;
+    public static ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // simplify insert of headNode
+        ListNode dummyNode = new ListNode(0);
+        ListNode curNode = head;
+        dummyNode.next = head;
+        while (curNode.next != null) {
+            if (curNode.val <= curNode.next.val) {
+                curNode = curNode.next;
+            } else {
+                ListNode targetNode = curNode.next;
 
-			// if less than head node ,become first
-			if (curNode.val < headNode.val) {
-				curNode.next = headNode;
-				headNode = curNode;
-				formerNode = formerNode.next;
-				curNode = formerNode.next;
+                //remove target node
+                curNode.next = curNode.next.next;
 
-				continue;
-			}
+                //user begin.node.next to complete insert
+                ListNode loopNode = dummyNode;
+                while (loopNode.next.val < targetNode.val) {
+                    loopNode = loopNode.next;
+                }
+                targetNode.next = loopNode.next;
+                loopNode.next = targetNode;
+            }
 
-			ListNode prevNode = headNode;
-			ListNode loopNode = prevNode.next;
-			while (loopNode != null) {
-				if (curNode.val < loopNode.val) {
-					prevNode.next = curNode;
-					curNode.next = loopNode;
-					break;
-				}
-				prevNode = loopNode;
-				loopNode = loopNode.next;
-			}
-			if (curNode.next == null) {
-				prevNode.next = curNode;
-			}
-			formerNode = formerNode.next;
-			curNode = formerNode.next;
-		}
-		return headNode;
+        }
+        return dummyNode.next;
 
-	}
+    }
 
-	public static void main(String[] args) {
-		ListNode head = new ListNode(4);
-		ListNode sec = new ListNode(2);
-		ListNode third = new ListNode(1);
-		ListNode four = new ListNode(3);
-		head.next = sec;
-		sec.next = third;
-		third.next = four;
-		insertionSortList(head);
-	}
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        ListNode sec = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode four = new ListNode(1);
+        head.next = sec;
+        sec.next = third;
+        third.next = four;
+        insertionSortList(head);
+    }
 }
