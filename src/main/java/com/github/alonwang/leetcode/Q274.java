bae511813efd3ce639d2fbb5b651d32178bcbf2b
weilong.wang <weilong.wang@jiamiantech.com>
2018-08-28 15:39:51 +0800

    h pointer

--- src/main/java/com/github/alonwang/leetcode/Q274.java
+++ src/main/java/com/github/alonwang/leetcode/Q274.java
@@ -3,28 +3,20 @@ package com.github.alonwang.leetcode;
 import java.util.Arrays;
 
 class Q274 {
-    public int hIndex(int[] citations) {
-        if (citations == null || citations.length == 0) return 0;
+	public int hIndex(int[] citations) {
+		if (citations == null || citations.length == 0)
+			return 0;
+		final int len = citations.length;
+		Arrays.sort(citations);
+		if (citations[0] >= len)
+			return len;
 
-        //TODO use self sort
-        final int len = citations.length;
-        Arrays.sort(citations);
-        if (citations[0] >= len) return len;
-
-
-        int low = 0;
-        int high = len - 1;
-        int h = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (citations[mid] <= (len - mid)) {
-                h = citations[mid];
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return h;
-
-    }
+		int h = 0;
+		int curH = 0;
+		for (int i = 0; i < len; i++) {
+			curH = Math.min(citations[i], len - i);
+			h = Math.max(curH, h);
+		}
+		return h;
+	}
 }
