9c7bf20599ad0828b65ab52db208bdd463edc3ba
alonwang <wangweilong1996@gmail.com>
2019-05-11 10:20:07 +0800

    冒泡

---
+++ src/main/java/com/github/alonwang/sort/BubblingSort.java
@@ -0,0 +1,27 @@
+package com.github.alonwang.sort;
+
+public class BubblingSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{43, 25, 3, 76, 33, 75, 22, 3454, 5};
+        sort(arr);
+        for (int n : arr) {
+            System.out.println(n);
+        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arrays, int i, int j) {
+        int temp = arrays[i];
+        arrays[i] = arrays[j];
+        arrays[j] = temp;
+    }
+}
