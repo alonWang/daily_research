2ca1a474f898d85f90dd6e3c23afeffc5ff69801
alonwang <wangweilong1996@gmail.com>
2019-05-16 21:09:34 +0800

    add explain

--- src/main/java/com/github/alonwang/leetcode/Q992.java
+++ src/main/java/com/github/alonwang/leetcode/Q992.java
@@ -1,7 +1,14 @@
 package com.github.alonwang.leetcode;
 
+/**
+ * 基于快排思想,左边以下标0(左边第一个为偶数的下标)开始,查找奇数.
+ * 右边以下标j(右边最后一个为奇数的下标),查找偶数, 交换.
+ * 循环结束后 偶数下标上全部为偶数,奇数下标上全部为奇数.
+ *
+ * 时间复杂度 O(N)
+ * 空间复杂度 O(1)
+ */
 public class Q992 {
-	// 左边扫描偶数,右边扫描奇数 基于快排思想
 	public int[] sortArrayByParityII(int[] A) {
 		if (A == null || A.length == 0) {
 			return A;
