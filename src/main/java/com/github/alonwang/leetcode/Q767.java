4ceb0a3c49c9057cb65c171532482c5e7e0e03ee
weilong.wang <weilong.wang@jiamiantech.com>
2019-05-21 18:25:21 +0800

    Q767 a better solution

--- src/main/java/com/github/alonwang/leetcode/Q767.java
+++ src/main/java/com/github/alonwang/leetcode/Q767.java
@@ -1,35 +1,42 @@
 package com.github.alonwang.leetcode;
 
-import java.util.Arrays;
-
+/**
+ * 关键点:
+ * 1. 桶排序
+ * 2. 设字符串长度为N,字符串中某个字符出现次数为C, 如果 C>(N+1)/2 就无法满足题目要求,只需要判断出现次数最多的那个字符是否满足就可以了
+ * 3. 先放置偶数位置,再放置奇数位置,**确保先放置出现次数最多的字符**.
+ */
 public class Q767 {
 	public String reorganizeString(String S) {
-		if (S == null || S.length() == 0) {
-			return "";
-		}
-		int N = S.length();
-		int ALPHA = 26;
-		int[][] buckets = new int[ALPHA][2];
-		for (int i = 0; i < ALPHA; i++) {
-			buckets[i][1] = 'a' + i;
-		}
+		int n = S.length(), maxCount = -1;
+		char maxLetter = 'a';
+		char[] res = new char[n];
+		int[] map = new int[26];
+
 		for (char c : S.toCharArray()) {
-			buckets[c - 'a'][0]++;
-		}
-		Arrays.sort(buckets, (a1, a2) -> a2[0] - a1[0]);
-		if (buckets[0][0] > (N + 1) / 2) {
-			return "";
-		}
-		char[] result = new char[N];
-		int j = 0;
-		for (int i = 0; i < ALPHA; i++) {
-			for (int k = 0; k < buckets[i][0]; k++, j += 2) {
-				if (j >= N) {
-					j = 1;
-				}
-				result[j] = (char) buckets[i][1];
+			map[c - 'a']++;
+			if (maxCount < map[c - 'a']) {
+				maxCount = map[c - 'a'];
+				maxLetter = c;
 			}
 		}
-		return new String(result);
+		if (maxCount > (n + 1) / 2)
+			return "";
+		int i = 0;
+		while (map[maxLetter - 'a'] > 0) {
+			res[i] = maxLetter;
+			i += 2;
+			map[maxLetter - 'a']--;
+		}
+		for (int j = 0; j < 26; j++) {
+			while (map[j] > 0) {
+				if (i >= n)
+					i = 1;
+				res[i] = (char) (j + 'a');
+				i = i + 2;
+				map[j]--;
+			}
+		}
+		return new String(res);
 	}
 }
