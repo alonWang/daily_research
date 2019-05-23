1a8933ede979774e5595f73d0d97880cc5a9610a
weilong.wang <weilong.wang@jiamiantech.com>
2019-05-23 09:38:45 +0800

    Q274 quick sort

--- src/main/java/com/github/alonwang/leetcode/Q274.java
+++ src/main/java/com/github/alonwang/leetcode/Q274.java
@@ -1,22 +1,48 @@
 package com.github.alonwang.leetcode;
 
-import java.util.Arrays;
-
+/**
+ * quick sort, reverse order,   step by step
+ */
 class Q274 {
-	public int hIndex(int[] citations) {
-		if (citations == null || citations.length == 0)
-			return 0;
-		final int len = citations.length;
-		Arrays.sort(citations);
-		if (citations[0] >= len)
-			return len;
+    public int hIndex(int[] c) {
+        if (c.length == 0) return 0;
+        int l = 0, h = c.length - 1;
+        while (l < h) {
+            int pos = partition(c, l, h);
+            if (c[pos] >= pos + 1) {
+                l = pos + 1;
+            } else {
+                h = pos - 1;
+            }
+        }
+        //all satisfy
+        if (l == c.length) {
+            return l;
+        }
 
-		int h = 0;
-		int curH = 0;
-		for (int i = 0; i < len; i++) {
-			curH = Math.min(citations[i], len - i);
-			h = Math.max(curH, h);
-		}
-		return h;
-	}
+        //after    l = pos + 1, loop end
+        if (c[l] >= l + 1) {
+            return l + 1;
+        } else {//after h = pos - 1, loop end
+            return l;
+        }
+
+    }
+
+
+    int partition(int[] c, int s, int e) {
+        int pivot = c[e];
+        int j = s;
+        for (int i = s; i < e; i++) {
+            if (c[i] > pivot) swap(c, i, j++);
+        }
+        swap(c, j, e);
+        return j;
+    }
+
+    void swap(int[] c, int i, int j) {
+        int t = c[i];
+        c[i] = c[j];
+        c[j] = t;
+    }
 }
