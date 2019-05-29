b54a483c1d9e0fb3d3e97195e757b94669ee33aa
weilong.wang <weilong.wang@jiamiantech.com>
2019-05-29 09:12:07 +0800

    Q324 quick sort & pretty slow

--- src/main/java/com/github/alonwang/leetcode/Q324.java
+++ src/main/java/com/github/alonwang/leetcode/Q324.java
@@ -6,15 +6,16 @@ package com.github.alonwang.leetcode;
  * @create: 2019-05-28 09:10
  **/
 public class Q324 {
-    public static void wiggleSort(int[] nums) {
-        if (nums == null || nums.length == 0) {
+    public void wiggleSort(int[] nums) {
+        if (nums == null || nums.length < 2) {
             return;
         }
         int N = nums.length;
         //quickSort
-        int mid = N / 2;
+        int mid = (N + 1) / 2;
         int balance = findKthNum(nums, 0, N - 1, mid);
         int lastEvenPos = N % 2 == 0 ? N - 2 : N - 1;
+        int lastOddPos = N % 2 == 0 ? N - 1 : N - 2;
         // 从左向右 奇数位(峰)  ,从右向左,偶位数(谷)   交换
         for (int i = 1, j = lastEvenPos; i < mid; i += 2, j -= 2) {
             swap(nums, i, j);
@@ -27,7 +28,7 @@ public class Q324 {
             }
         }
 
-        for (int i = lastEvenPos, j = i; i >= 0; i -= 2) {
+        for (int i = lastOddPos, j = i; i >= 0; i -= 2) {
             if (nums[i] == balance) {
                 swap(nums, i, j);
                 j -= 2;
@@ -42,32 +43,41 @@ public class Q324 {
      * @param k
      * @return
      */
-    public static int findKthNum(int[] nums, int l, int h, int k) {
-        //单边排 必须确定哨兵位置
-        int balance = nums[h];
-        int i = l;
-        for (int j = l; j < h; j++) {
-            if (nums[j] < balance) {
-                swap(nums, j, i++);
+    public int findKthNum(int[] nums, int l, int h, int k) {
+        int base = nums[h];
+        int L = l, H = h;
+        for (; L <= H; ) {
+            for (; L <= H && nums[H] > base; ) {
+                H--;
             }
+            for (; L <= H && nums[L] < base; ) {
+                L++;
+            }
+            if (L <= H) {
+                swap(nums, L, H);
+                L++;
+                H--;
+            }
+
         }
-        swap(nums, i, h);
-        if (i < k) {
-            return findKthNum(nums, l + 1, h, k);
-        } else if (i > k) {
-            return findKthNum(nums, l, h - 1, k);
+        if (H - l + 1 >= k) {
+            return findKthNum(nums, l, H, k);
+        } else if (L - 1 - l + 1 >= k) {
+            return base;
         } else {
-            return nums[i];
+            return findKthNum(nums, L, h, k - (L - 1 - l + 1));
         }
+
     }
 
-    private static void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
     public static void main(String[] args) {
-        wiggleSort(new int[]{1, 1, 2, 1, 2, 2, 1});
+        Q324 q = new Q324();
+        q.wiggleSort(new int[]{1, 5, 1, 1, 6, 4});
     }
 }
