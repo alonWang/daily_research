e07f5455110be5b9b82243c2a777cd2f1cb259c4
alonwang <wangweilong1996@gmail.com>
2019-09-03 23:41:36 +0800

    optimize

--- src/main/java/com/github/alonwang/lang/MultiThreadPrinter.java
+++ src/main/java/com/github/alonwang/lang/MultiThreadPrinter.java
@@ -4,40 +4,36 @@ package com.github.alonwang.lang;
  * 两个线程交替打印数字
  */
 public class MultiThreadPrinter {
-    static class ThreadPrinter implements Runnable {
-        private int num;
-        private final int endInclude;
-        private final Object lock;
+    private int num;
+    private final int endInclude;
+    private final Object lock = new Object();
 
-        public ThreadPrinter(int num, int endInclude, Object lock) {
-            this.num = num;
-            this.endInclude = endInclude;
-            this.lock = lock;
-        }
+    public MultiThreadPrinter(int num, int endInclude) {
+        this.num = num;
+        this.endInclude = endInclude;
+    }
 
-        @Override
-        public void run() {
-            while (num <= endInclude) {
-                synchronized (lock) {
-                    System.out.println(Thread.currentThread().getName() + " : " + num);
-                    num += 2;
-                    lock.notify();
-                    if (num <= endInclude) {
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+    public void print() {
+        while (num <= endInclude) {
+            synchronized (lock) {
+                System.out.println(Thread.currentThread().getName() + " : " + num++);
+                lock.notify();
+                if (num <= endInclude) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             }
         }
     }
 
+
     public static void main(String[] args) {
-        Object lock = new Object();
-        new Thread(new ThreadPrinter(0, 100, lock), "even").start();
-        new Thread(new ThreadPrinter(1, 100, lock), "odd").start();
+        MultiThreadPrinter printer = new MultiThreadPrinter(0, 100);
+        new Thread(printer::print, "even").start();
+        new Thread(printer::print, "odd").start();
     }
 
 }
