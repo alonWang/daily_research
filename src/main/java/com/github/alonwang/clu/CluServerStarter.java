7d02ffdfe578e47fc12610e9c6b864e78e3afaa5
weilong.wang <weilong.wang@jiamiantech.com>
2019-11-15 16:28:51 +0800

    feat: clu part

---
+++ src/main/java/com/github/alonwang/clu/CluServerStarter.java
@@ -0,0 +1,60 @@
+package com.github.alonwang.clu;
+
+import com.github.alonwang.clu.handler.BusinessHandler;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+import lombok.extern.java.Log;
+
+/**
+ * @description:
+ * @author: alonwang
+ * @create: 2019-11-15 08:47
+ **/
+@Log
+public class CluServerStarter {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(10);
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new ChannelHandler() {
+                    @Override
+                    public void handlerAdded(ChannelHandlerContext channelHandlerContext) throws Exception {
+                        log.info(channelHandlerContext.name() + " handler add");
+                    }
+
+                    @Override
+                    public void handlerRemoved(ChannelHandlerContext channelHandlerContext) throws Exception {
+                        log.info(channelHandlerContext.name() + " handler removed");
+                    }
+
+                    @Override
+                    public void exceptionCaught(ChannelHandlerContext channelHandlerContext, Throwable throwable) throws Exception {
+                        log.info(channelHandlerContext.name() + " handler exception " + throwable);
+                    }
+                }).childHandler(new ChannelInitializer<NioSocketChannel>() {
+            @Override
+            protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                nioSocketChannel.pipeline().addLast(
+                        new HttpServerCodec(),
+                        new HttpObjectAggregator(65536),
+                        new WebSocketServerProtocolHandler("/"),
+                        new BusinessHandler()
+                );
+            }
+        });
+
+        serverBootstrap.bind(8080);
+    }
+}
---
+++ src/main/java/com/github/alonwang/clu/handler/BusinessHandler.java
@@ -0,0 +1,28 @@
+package com.github.alonwang.clu.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+/**
+ * @description:
+ * @author: alonwang
+ * @create: 2019-11-15 14:31
+ **/
+public class BusinessHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println(msg.text());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+    }
+}
