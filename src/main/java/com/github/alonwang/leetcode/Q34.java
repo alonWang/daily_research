90523bfc31cb750c260882a7fe0c4d79cc1de5eb
alonwang <wangweilong1996@gmail.com>
2019-12-08 23:23:51 +0800

    feat: binary range search

--- src/main/java/com/github/alonwang/leetcode/Q34.java
+++ src/main/java/com/github/alonwang/leetcode/Q34.java
@@ -1,28 +1,41 @@
 package com.github.alonwang.leetcode;
 
 class Q34 {
-    public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
-        int lastPos=guess(0,n,target,nums);
-        if(-1!=lastPos&&nums[lastPos]==target){
-            //向前查找
-            int firstPos=lastPos;
-            while(firstPos>0&&nums[firstPos-1]==target) firstPos--;
-            return new int[]{firstPos,lastPos};
-        }else
-            return new int[]{-1,-1};
+	public int[] searchRange(int[] nums, int target) {
+		int n = nums.length;
+		int lastPos = guessLast(0, n, target, nums);
+		int firstPos = guessFirst(0, n, target, nums);
+		if (-1 != lastPos && nums[lastPos] == target && -1 != firstPos
+				&& nums[firstPos] == target) {
+			return new int[] { firstPos, lastPos };
+		} else
+			return new int[] { -1, -1 };
     }
 
-    //最后一个符合条件的元素的下标
-    public int guess(int L, int R, int target, int[]nums){
-        int ans=-1;
-        while(L<R){
-            int mid=(L+R)/2;
-            if(nums[mid]<=target){
-                ans=mid;
-                L=mid+1;
-            }else{
-                R=mid;
+	// 最后一个符合条件的元素的下标
+	public int guessLast(int L, int R, int target, int[] nums) {
+		int ans = -1;
+		while (L < R) {
+			int mid = (L + R) / 2;
+			if (nums[mid] <= target) {
+				ans = mid;
+				L = mid + 1;
+			} else {
+				R = mid;
+			}
+		}
+		return ans;
+	}
+
+	public int guessFirst(int L, int R, int target, int[] nums) {
+		int ans = -1;
+		while (L < R) {
+			int mid = (L + R) / 2;
+			if (nums[mid] < target) {
+				L = mid + 1;
+			} else {
+				ans = mid;
+				R = mid;
             }
         }
         return ans;
