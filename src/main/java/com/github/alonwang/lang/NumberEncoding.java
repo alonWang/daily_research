2be77185b0593006cb958c3f2767aeec60785c80
alonwang <wangweilong1996@gmail.com>
2020-07-10 15:36:51 +0800

    migrate from Maven to Gradle

---
+++ src/main/java/com/github/alonwang/lang/NumberEncoding.java
@@ -0,0 +1,15 @@
+package com.github.alonwang.lang;
+
+/**
+ * @author alonwang
+ * @date 2020/7/2 15:31
+ * @detail
+ */
+public class NumberEncoding {
+
+    public static void main(String[] args) {
+        System.out.println((3.14f+1e20)-1e20);
+        System.out.println(3.14+(1e20-1e20));
+    }
+
+}
