db121c10a61d674a228c8227e8200e8f4b1528da
alonwang <wangweilong1996@gmail.com>
2020-12-16 21:59:31 +0800

    设置项的部分代码

---
+++ daily/src/main/java/com/github/alonwang/ddd/entity/Settings.java
@@ -0,0 +1,23 @@
+package com.github.alonwang.ddd.entity;
+
+import com.github.alonwang.ddd.vo.SettingElement;
+
+import java.util.List;
+
+/**
+ * @author alonwang
+ * @date 2020/12/16 21:47
+ */
+public class Settings {
+    /**
+     * 用户id
+     */
+    private long userId;
+    /**
+     * 设置项列表
+     */
+    private List<SettingElement> elements;
+
+
+
+}
