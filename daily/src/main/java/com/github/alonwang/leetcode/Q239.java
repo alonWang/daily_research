c8eff466cf0c10c65019d8975fad4dacbf723576
alonwang <wangweilong1996@gmail.com>
2021-02-19 22:13:53 +0800

    滑动窗口最大值

---
+++ daily/src/main/java/com/github/alonwang/leetcode/Q239.java
@@ -0,0 +1,46 @@
+package com.github.alonwang.leetcode;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 借助队列维护窗口内的大小次序,并通过窗口优化队列移除.
+ * @author alonwang
+ * @date 2021/2/19 9:44 下午
+ */
+public class Q239 {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (k == 1) {
+                return nums;
+            }
+            int[] result = new int[nums.length - k + 1];
+            //元素,出现次数
+            Map<Integer, Integer> windows = new HashMap<>(k);
+            //滑动过程中维护 大小
+            PriorityQueue<Integer> pq = new PriorityQueue<>(k, Comparator.reverseOrder());
+            int left = 0, right = 0;
+            while (right < nums.length) {
+                int added = nums[right];
+                windows.put(added, windows.getOrDefault(added, 0) + 1);
+                pq.add(added);
+                right++;
+
+                if (right - left == k) {
+                    int removed =nums[left];
+                    while(windows.getOrDefault(pq.peek(),0)<=0){
+                        pq.poll();
+                    }
+                    result[left] = pq.peek();
+                    windows.put(removed,windows.getOrDefault(removed,0)-1);
+                    left++;
+                }
+            }
+            return result;
+        }
+    }
+
+
+}
