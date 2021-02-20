7a3cd2309681705440c2154627fdc9bffee4f470
alonwang <wangweilong1996@gmail.com>
2021-02-20 09:40:49 +0800

    滑动窗口最大值

--- daily/src/main/java/com/github/alonwang/leetcode/Q239.java
+++ daily/src/main/java/com/github/alonwang/leetcode/Q239.java
@@ -7,6 +7,7 @@ import java.util.PriorityQueue;
 
 /**
  * 借助队列维护窗口内的大小次序,并通过窗口优化队列移除.
+ *
  * @author alonwang
  * @date 2021/2/19 9:44 下午
  */
@@ -24,23 +25,35 @@ public class Q239 {
             int left = 0, right = 0;
             while (right < nums.length) {
                 int added = nums[right];
-                windows.put(added, windows.getOrDefault(added, 0) + 1);
+                windows.compute(added, (key, v) -> {
+                    if (v == null) {
+                        v = 0;
+                    }
+                    return v + 1;
+                });
                 pq.add(added);
                 right++;
 
                 if (right - left == k) {
-                    int removed =nums[left];
-                    while(windows.getOrDefault(pq.peek(),0)<=0){
+                    int removed = nums[left];
+
+                    while (windows.getOrDefault(pq.peek(), 0) <= 0) {
                         pq.poll();
                     }
                     result[left] = pq.peek();
-                    windows.put(removed,windows.getOrDefault(removed,0)-1);
                     left++;
+                    windows.compute(removed, (key, v) -> {
+                        if (v == 1) {
+                            return null;
+                        }
+                        return v - 1;
+                    });
+
+
                 }
             }
             return result;
         }
     }
-
 
 }
