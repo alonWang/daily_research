066f3ba3c366f772824fb86a76ded1e224497022
alonwang <wangweilong1996@gmail.com>
2021-03-01 18:26:19 +0800

    leetcode

--- daily/src/main/java/com/github/alonwang/leetcode/Q174.java
+++ daily/src/main/java/com/github/alonwang/leetcode/Q174.java
@@ -10,7 +10,7 @@ public class Q174 {
         int min;
         int M;
         int N;
-        int dp[][];
+
         public int calculateMinimumHP(int[][] dungeon) {
             M=dungeon.length;
             N=dungeon[0].length;
@@ -19,16 +19,10 @@ public class Q174 {
             }
             min=Integer.MIN_VALUE;
             map=dungeon;
-            dp=new int[M][N];
+
             int x=0,y=0;
             int curHp=dungeon[0][0];
             int curMin=Math.min(dungeon[0][0],0);
-            for(int i=0;i<M;i++){
-                for(int j=0;j<N;j++){
-                    dp[i][j]=Integer.MIN_VALUE;
-                }
-            }
-            dp[0][0]=curMin;
             deepSearch(curHp,curMin,x,y+1);
             deepSearch(curHp,curMin,x+1,y);
 
@@ -41,7 +35,6 @@ public class Q174 {
             }
             curHp+=map[x][y];
             curMin=Math.min(curHp,curMin);
-            dp[x][y]=Math.max(dp[x][y],curMin);
             if(x==M-1&&y==N-1){
                 min=Math.max(min,curMin);
                 return;
@@ -51,23 +44,45 @@ public class Q174 {
             if(curMin<min){
                 return;
             }
-            if(x+1<M&&y+1<N&&curMin<dp[x+1][y]&&curMin<dp[x][y+1]){
-                return;
-            }else if(x+1<M&&curMin<dp[x+1][y]){
-                return;
-            }else if(y+1<N&&curMin<dp[x][y+1]){
-                return;
-            }
             deepSearch(curHp,curMin,x,y+1);
             deepSearch(curHp,curMin,x+1,y);
-
-
-
         }
     }
 
+    /**
+     * 动态规划解法
+     * dp[i][j] 从(i,j)到终点所需最小hp.
+     * dp[i][j]=max(1,min(dp[i][j+1],dp[i+1,j])-dungeon[i][j] )
+     * 按照这个定义,终点的右侧和下侧只需要为最小血量1.而dp[~][N+1]和dp[M][~]只需要为一个最大的无效值,确保其不被选择到即可
+     * @param dungeon
+     * @return
+     */
+    public int calculateMinimumHP2(int[][] dungeon) {
+        int M = dungeon.length;
+        int N = dungeon[0].length;
+        //从(i,j)到终点所需最小HP 从右下开始计算
+        int[][] dp = new int[M + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            dp[i][N] = Integer.MAX_VALUE;
+        }
+        for (int j = 0; j < N; j++) {
+            dp[M][j] = Integer.MAX_VALUE;
+        }
+        //终点的右/下 无效,用1表示从终点走到那里只需要1
+        dp[M][N - 1] = dp[M - 1][N] = 1;
+        for(int i=M-1;i>=0;i--){
+            for (int j=N-1;j>=0;j--){
+                //最优解必定来自右侧和下侧中的最小者.
+                //如果当前格子扣血(<0) 需要的最小值是增加的,反之相同,所以直接减去格子值即可
+                //最后用1保证加血时不能超加
+                int min=Math.min(dp[i][j+1],dp[i+1][j]);
+                dp[i][j]=Math.max(min-dungeon[i][j],1);
+            }
+        }
+        return dp[0][0];
+    }
     public static void main(String[] args) {
         int[][] map={{1,-3,3},{0,-2,0},{-3,-3,-3}};
-        new Solution().calculateMinimumHP(map);
+        System.out.println(new Solution().calculateMinimumHP(map));
     }
 }
--- daily/src/main/java/com/github/alonwang/leetcode/Q239.java
+++ daily/src/main/java/com/github/alonwang/leetcode/Q239.java
@@ -1,18 +1,25 @@
 package com.github.alonwang.leetcode;
 
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue;
 
 /**
- * 借助队列维护窗口内的大小次序,并通过窗口优化队列移除.
- *
  * @author alonwang
  * @date 2021/2/19 9:44 下午
  */
 public class Q239 {
     class Solution {
+        /**
+         * 借助PriorityQueue维护窗口内的大小次序,并通过窗口优化队列移除.O(nlogn)
+         *
+         * @param nums
+         * @param k
+         * @return
+         */
         public int[] maxSlidingWindow(int[] nums, int k) {
             if (k == 1) {
                 return nums;
@@ -50,6 +57,42 @@ public class Q239 {
                     });
 
 
+                }
+            }
+            return result;
+        }
+
+        /**
+         * 事实: 只需要知道窗口中的最大值
+         * 思路: 自己维护窗口中的最大值
+         * 实现: 通过双端队列 队头 最大值下标 <- 队尾 最小值下标
+         * 窗口右侧右移,新增元素,从队尾向队头,将所有小于新增值的元素移除掉,之后在队尾加上新增元素
+         * 窗口左侧右移,移除元素 如果队头元素等于移除元素,移除队头.
+         * @param nums
+         * @param k
+         * @return
+         */
+        public int[] maxSlidingWindow2(int[] nums, int k) {
+            if (k == 1) {
+                return nums;
+            }
+            int[] result = new int[nums.length - k + 1];
+            //最大<-最小 元素的下标
+            Deque<Integer> queue = new LinkedList<>();
+            int left = 0, right = 0;
+            while (right < nums.length) {
+                int added = nums[right];
+                while (!queue.isEmpty() && nums[queue.peekLast()] < added) {
+                    queue.pollLast();
+                }
+                queue.addLast(right);
+                right++;
+                if (right - left == k) {
+                    result[left] = nums[queue.peekFirst()];
+                    if (left == queue.peekFirst()) {
+                        queue.pollFirst();
+                    }
+                    left++;
                 }
             }
             return result;
