3cada83b87f5ddb1903157cd5c1a5bbc7fb819d7
alonwang <wangweilong1996@gmail.com>
2021-03-05 17:04:14 +0800

    接雨水

---
+++ daily/src/main/java/com/github/alonwang/leetcode/Q42.java
@@ -0,0 +1,14 @@
+package com.github.alonwang.leetcode;
+
+/**
+ * @author alonwang
+ * @date 2021/3/5 4:29 下午
+ */
+public class Q42 {
+    class Solution {
+        public int trap(int[] height) {
+
+            return -1;
+        }
+    }
+}
