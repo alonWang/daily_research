bb2fe98dd73001077910f135db98e47bca1054ef
alonwang <wangweilong1996@gmail.com>
2021-03-15 19:21:54 +0800

    1025

---
+++ daily/src/main/java/com/github/alonwang/leetcode/Q1025.java
@@ -0,0 +1,16 @@
+package com.github.alonwang.leetcode;
+
+/**
+ * @author alonwang
+ * @date 2021/3/15 7:21 下午
+ */
+public class Q1025 {
+    /**
+     * 数学题
+     */
+    class Solution {
+        public boolean divisorGame(int N) {
+            return N/2==0;
+        }
+    }
+}
